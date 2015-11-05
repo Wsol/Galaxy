80adf6f91cf10fb7fa64fa90698a2fb892d91cb5
Wsol <wendy_liz15@hotmail.com>
2015-11-05 00:38:21 -0600

    Avance hasta 02 de Noviembre
    Se tiene la nave que se mueve y el disparo.

---
+++ Assets/Script/Move_Shot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Shot : MonoBehaviour
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;        	
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		
+        if(moveHorizontal >= 0.01f || moveHorizontal <= 0.01f)
+        {
+            gameObject.transform.Translate(new Vector3(moveHorizontal, 0.0f, 0.0f));
+        }        		
+	}
+}
